3171f29ec362970c22f3e899f61eab7f715144bb
courtcoder <courtney1187@icloud.com>
2022-07-10 14:25:07 -0400

    added files

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api/user-routes");
     
-router.use("/api", apiRoutes);
+router.use("./api", apiRoutes);
+
+const homeRoutes = require("./home");
+
+router.use("./home-routes.js", homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,5 +19,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
